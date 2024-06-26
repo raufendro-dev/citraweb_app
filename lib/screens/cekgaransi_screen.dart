f4d7d25177e90acb57f4be5d2492d1224e42602b
raufendro-dev <raufendro@gmail.com>
2024-06-26 17:13:40 +0700

    up

--- lib/screens/cekgaransi_screen.dart
+++ lib/screens/cekgaransi_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:mikrotik/constant/config.dart';
+import 'package:mikrotik/services/auth_service.dart';
 
 class CekgaransiScreen extends StatefulWidget {
   const CekgaransiScreen({Key? key}) : super(key: key);
@@ -8,8 +13,25 @@ class CekgaransiScreen extends StatefulWidget {
 }
 
 class _CekgaransiScreenState extends State<CekgaransiScreen> {
+  List device = [];
+  Future cekgaransi() async {
+    var bodyPost = jsonEncode({
+      "key": "0cc7da679bea04fea453c8062e06514d",
+      "ip": await AuthService.getIPAddress(),
+      "devices": ""
+    });
+    var res = await http.post(
+      Uri.parse(Config.baseUrlApi + 'app-api//cekgaransi/'),
+      body: bodyPost,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold( 
+      appBar: AppBar(
+        title: const Text('Cek Garansi'),
+      ),
+    );
   }
 }
